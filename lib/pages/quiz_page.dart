485f133785c6e841cd36745f29699c386f905f12
DeadAirX <djdivinity7@gmail.com>
2018-04-25 07:44:11 -0500

    Add basic quiz

---
+++ lib/pages/quiz_page.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+import '../utils/question.dart';
+import '../utils/quiz.dart';
+
+import '../UI/answer_button.dart';
+import '../UI/question_text.dart';
+import '../UI/input_response_overlay.dart';
+
+class QuizPage extends StatefulWidget {
+  @override
+  State createState() => new QuizPageState();
+}
+
+class QuizPageState extends State<QuizPage> {
+
+  Question currentQuestion;
+  Quiz quiz = new Quiz([
+    new Question("Elon Musk is human", false),
+    new Question("Pizza is healthy", false),
+    new Question("Flutter is awesome", true)
+  ]);
+  String questionText;
+  int questionNumber;
+  bool isCorrect;
+  bool overlayVisible = false;
+
+  @override
+  void initState(){
+    super.initState();
+    currentQuestion = quiz.nextQuestion;
+    questionText = currentQuestion.question;
+    questionNumber = quiz.questionNumber;
+  }
+
+  void handleAnswer(bool answer) {
+    isCorrect = (currentQuestion.answer == answer);
+    quiz.answer(isCorrect);
+    this.setState(() {
+      overlayVisible = true;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context){
+    return new Stack(
+      fit: StackFit.expand,
+      children: <Widget>[
+        new Column(
+          children: <Widget>[
+            new AnswerButton(true, () => handleAnswer(true)),
+            new QuestionText(questionText, questionNumber),
+            new AnswerButton(false, () => handleAnswer(false)),
+          ],
+        ),
+        overlayVisible == true ? new InputResponseOverlay(isCorrect) : new Container()
+      ],
+    );
+  }
+}
